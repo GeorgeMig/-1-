742c7d5bccbe4c6da493fad2dbe0e88f70a2bbbd
George_M <msgoha555@yandex.ru>
2022-04-30 11:32:29 +0300

    Example 005 Condition IF else

---
+++ Example005_ConditionIFelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "питер")
+{
+    Console.WriteLine("Здраствуй, мой мальчик! Все борешься с ленью?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
